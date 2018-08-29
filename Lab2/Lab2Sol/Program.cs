aee1f485b4a6085b360f636671dbac436bd3ab15
Alex Smith <gn1k@hotmail.com>
2018-08-28 20:51:32 -0600

    Added gauss sum.

---
+++ Lab2/Lab2Sol/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading;
+
+namespace Lab2Sol
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            for (int i = 1; i <= 100; i++)
+            {
+                sum += i;
+            }
+            Console.WriteLine("The sum is " + sum);
+            Thread.Sleep(5000);
+        }
+    }
+}
